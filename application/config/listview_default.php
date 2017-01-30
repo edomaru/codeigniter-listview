069f18ee71205f854302c2dce0d629615806efc6
hamedhossani <hamed_hossani@yahoo.com>
2017-01-30 18:31:24 +0000

    Add the default values for responsive template config/listview_default.php
    There are more beautiful appearance.
    The template is responsive.
    
    http://www.w3schools.com/bootstrap/bootstrap_pagination.asp

--- application/config/listview_default.php
+++ application/config/listview_default.php
@@ -8,10 +8,24 @@ $config['default'] = array(
 		'view'				=> "_listview/_show_record"
 	),
 	'pagination' => array(
-		"full_tag_open" 	=> '<div id="paging">' . PHP_EOL,
-		"full_tag_close" 	=> '</div>'		
+		"full_tag_open" 			=> '<div class="col-md-12" style="text-align:center;"><ul class="pagination" style="margin:0px;"> ' . PHP_EOL,
+		"full_tag_close" 			=> '</ul></div>',
+		"use_page_numbers" 			=> TRUE,
+		"prev_link"					=> '&laquo;',
+		"next_link" 				=> '&raquo;',
+		"first_tag_open"			=>"<li>",
+		"first_tag_close"			=>"</li>",
+		"next_tag_open"				=>'<li>',
+		"next_tag_close"			=>'</li>',
+		'prev_tag_open'				=>'<li>',
+		'prev_tag_close'			=>'</li>',
+		'num_tag_open'				=>'<li>',
+		'num_tag_close'				=>'</li>',
+		'cur_tag_open'				 => ' <li class="active"><a href="#">',
+		'cur_tag_close' 			=> '</a></li>'			
 	),
 	'table' => array(
+		'attr'					=>array('class'=>'table table-responsive table-striped table-hover ')
 		/*'action'			=> array(
 			'attr' => array('style' => 'width:40px;'),
 			'position' => 'first', 
